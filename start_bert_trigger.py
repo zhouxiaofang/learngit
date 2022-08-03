fec54969bf0577283b596bb4906ba60dc1174466
zhouxiaofang <975210651@qq.com>
2022-08-03 13:48:36 +0800

    执行器文件归档到源码执行仓库里

--- start_bert_trigger.py
+++ start_bert_trigger.py
@@ -5,7 +5,7 @@ import mlflow
 if __name__ == "__main__":
     experiment_id = mlflow.create_experiment("bert-experiment")
 
-    command_str = "ssh zhoufang@10.101.7.1 '/home/common/software/anaconda3/bin/python3 -u /home/zhoufang/yu_bert_train_trigger.py'"
+    command_str = "ssh zhoufang@10.101.7.1 '/home/common/software/anaconda3/bin/python3 -u /home/zhoufang/mpt-wx/yu_bert_train_trigger.py'"
     with mlflow.start_run(experiment_id=experiment_id):
         mlflow.log_param("alpha", "5.0")
         mlflow.log_param("l1_ratio", "0.1")
