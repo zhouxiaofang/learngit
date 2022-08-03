bc484dab6ec95ee768a5e319647d24f1837c4161
zhouxiaofang <975210651@qq.com>
2022-08-03 10:42:38 +0800

    bert is track in mlflow

--- start_bert_trigger.py
+++ start_bert_trigger.py
@@ -1,6 +1,10 @@
 import os
 import mlflow
 
+mlflow.set_tracking_uri("http://10.101.4.2:5000")
+mlflow.set_experiment("my-experiment-bert")
+
+
 if __name__ == "__main__":
     command_str = "ssh zhoufang@10.101.7.1 '/home/common/software/anaconda3/bin/python3 -u /home/zhoufang/yu_bert_train_trigger.py'"
     with mlflow.start_run():
