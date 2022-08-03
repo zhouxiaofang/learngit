e714c966055c552aa4bf3e059d8dc79fecbb3628
zhouxiaofang <975210651@qq.com>
2022-08-03 11:12:53 +0800

    实验id

--- start_bert_trigger.py
+++ start_bert_trigger.py
@@ -3,7 +3,7 @@ import mlflow
 
 
 if __name__ == "__main__":
-    experiment_id = mlflow.create_experiment("bert-demo")
+    experiment_id = mlflow.create_experiment("bert-experiment")
 
     command_str = "ssh zhoufang@10.101.7.1 '/home/common/software/anaconda3/bin/python3 -u /home/zhoufang/yu_bert_train_trigger.py'"
     with mlflow.start_run(experiment_id=experiment_id):
