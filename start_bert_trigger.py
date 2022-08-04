f7a5b812a5be27cdca539a18440c64d3de7d69fc
zhouxiaofang <975210651@qq.com>
2022-08-04 15:33:26 +0800

    更新指标记录

--- start_bert_trigger.py
+++ start_bert_trigger.py
@@ -7,10 +7,10 @@ if __name__ == "__main__":
 
     command_str = "ssh zhoufang@10.101.7.1 '/home/common/software/anaconda3/bin/python3 -u /home/zhoufang/mpt-wx/yu_bert_train_trigger.py'"
     with mlflow.start_run(experiment_id=experiment_id):
-        mlflow.log_param("alpha", "5.0")
-        mlflow.log_param("l1_ratio", "0.1")
-        mlflow.log_metric("rmse", "0.859")
-        mlflow.log_metric("r2", "0.047")
-        mlflow.log_metric("mae", "0.648")
+        mlflow.log_param("iteration", "100")
+        mlflow.log_param("batch size", "8")
+        mlflow.log_metric("learning rate", "9.173E-05")
+        mlflow.log_metric("lm loss", "8.050369E+00")
+        mlflow.log_metric("sop loss", "7.277749E-01")
         os.system(command_str)
         
