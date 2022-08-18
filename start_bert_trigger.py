42a71210e1659a858edc7ba8962cd10e0a68d876
zhouxiaofang <975210651@qq.com>
2022-08-18 16:17:38 +0800

    参数

--- start_bert_trigger.py
+++ start_bert_trigger.py
@@ -1,16 +1,19 @@
 import os
 import mlflow
+from decimal import Decimal
 
 
 if __name__ == "__main__":
     experiment_id = mlflow.create_experiment("BertML-experiment3")
+    
+    rate = float(Decimal(0.9173))
 
     command_str = "ssh zhoufang@10.101.7.1 '/home/common/software/anaconda3/bin/python3 -u /home/zhoufang/mpt-wx/yu_bert_train_trigger.py'"
     with mlflow.start_run(experiment_id=experiment_id):
         mlflow.log_param("iteration", "100")
         mlflow.log_param("batch size", "8")
-        mlflow.log_metric("learning rate", "0.9173")
-        mlflow.log_metric("lm loss", "0.8050369")
-        mlflow.log_metric("sop loss", "0.7277749")
+        mlflow.log_metric("learning rate", rate)
+        mlflow.log_metric("lm loss", rate)
+        mlflow.log_metric("sop loss", rate)
         os.system(command_str)
         
